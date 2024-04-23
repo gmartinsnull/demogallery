67e55af317aa6585f4631ae452973f1b898ef956
gmartinsnull <gmartins.dev@gmail.com>
2024-04-22 18:59:28 -0700

    ensure analytics only runs on server side

--- src/server/analytics.ts
+++ src/server/analytics.ts
@@ -1,14 +1,14 @@
-// app/posthog.js
-import { PostHog } from 'posthog-node'
+import "server-only";
+import { PostHog } from "posthog-node";
 
 function serverSideAnalytics() {
   const posthogClient = new PostHog(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
     host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
     flushAt: 1,
-    flushInterval: 0
-  })
-  return posthogClient
+    flushInterval: 0,
+  });
+  return posthogClient;
 }
 
-export const analyticsServerClient = serverSideAnalytics()
+export const analyticsServerClient = serverSideAnalytics();
 export default serverSideAnalytics;
